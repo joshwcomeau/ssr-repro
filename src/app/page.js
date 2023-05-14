c7de9378048f96c4a7c77621e8484173c2049a31
Joshua Comeau <joshwcomeau@gmail.com>
2023-05-14 17:29:14 -0400

    Add issue reproduction

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Home() {
-  return <p>Hello world!</p>;
+  // Steps to repro:
+  // 1. Start a dev server, note the server-logged error
+  // 2. Delete this next line of code:
+  window.localStorage.getItem('hello');
+
+  return <p>Hi</p>;
 }
 
 export default Home;
